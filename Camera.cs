d15cbe4ffcf006e3c543cd38f8832516a481fb2c
thedeadleaff <66294406+thedeadleaff@users.noreply.github.com>
2021-01-12 13:05:10 -0500

    Add files via upload
    Code written to allow the player movements in Unity

---
+++ Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float speedP = 2.0f;
+    public float speedQ = 2.0f;
+
+    private float ye = 0.0f;
+    private float haw = 0.0f;
+
+    void Update()
+    {
+        ye += speedP * Input.GetAxis("Mouse X");
+        haw += speedQ * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(haw, ye, 0.0f);
+    }
+}
---
+++ PlayerCam.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float CurrentX = 0.0f;
+    private float CurrentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+
+    private void Start ()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update ()
+    {
+        CurrentX += Input.GetAxis("Mouse X");
+        CurrentY += Input.GetAxis("Mouse Y");
+
+        CurrentYv = Mathf.Clamp(CurrentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate ()
+    {
+        Vector3 dir = new Vector3(0,0,-distance);
+        Quaternion rotation = Quaternion.Euler(CurrentY, CurrentX,0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
